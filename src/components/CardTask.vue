7cfeddd69d4e27f68ff8271233d90bfcf284e773
david <deivid.develop@gmail>
2020-08-19 12:01:03 +0200

    Removing task in firebase from front

--- src/components/CardTask.vue
+++ src/components/CardTask.vue
@@ -1,24 +1,18 @@
 <template>
   <div class="q-pa-md q-gutter-sm">
-    <q-card class="row q-pa-md q-gutter-sm"
-      dark flat bordered v-for="(item, index) in tasks" :key="index">
-      <q-toggle
-        @click="item.state = !item.state"
-        v-model="item.state"
-        color="green"
-      />
-      <q-card-section
-        :class="item.state ? 'through' : ''"
-        class="col"
-        v-html="item.title" />
+    <q-card
+      class="row q-pa-md q-gutter-sm"
+      dark
+      flat
+      bordered
+      v-for="(item, index) in tasks"
+      :key="index"
+    >
+      <q-toggle @click="item.state = !item.state" v-model="item.state" color="green" />
+      <q-card-section :class="item.state ? 'through' : ''" class="col" v-html="item.title" />
 
-
-      <q-btn class="q-pa-xs"
-        rounded color="primary"
-        @click="item.state = !item.state">Edit</q-btn>
-      <q-btn class="q-pa-xs"
-        rounded color="red"
-        @click="clean(index)">Delete</q-btn>
+      <q-btn class="q-pa-xs" rounded color="primary" @click="item.state = !item.state">Edit</q-btn>
+      <q-btn class="q-pa-xs" rounded color="red" @click="clean(index, item.id)">Delete</q-btn>
     </q-card>
   </div>
 </template>
@@ -27,53 +21,62 @@
 import { db } from "boot/firebase";
 export default {
   components: {},
-  name: 'CardTask',
-  props: ['tasks',],
+  name: "CardTask",
+  props: ["tasks"],
   data() {
     return {
       state: false,
-      tasks: []
-    }
+      tasks: [],
+    };
   },
-  created(){
+  created() {
     this.listTasks();
   },
   methods: {
-    async listTasks(){
+    async listTasks() {
       try {
-        const resDb = await db.collection('Tasks').get();
-        resDb.forEach(res => {
+        const resDb = await db.collection("Tasks").get();
+        resDb.forEach((res) => {
           const task = {
             id: res.id,
             title: res.data().title,
-            state: res.data().state
-          }
+            state: res.data().state,
+          };
           this.tasks.push(task);
         });
       } catch (error) {
         console.log(error);
       }
     },
-    clean(index) {
-      this.$q.dialog({
-        dark: true,
-        title: 'Confirm',
-        message: 'Are you sure you want to delete?',
-        color: 'warning',
-        cancel: true,
-        persistent: true
-      }).onOk(() => {
-        this.$q.notify({
-        message: 'Delete task',
-        color: 'red',
-        textColor: 'white',
-        icon: 'cloud_done'
-      })
-        this.tasks.splice(index, 1)
-      })
-    }
-  }
-}
+    clean(index, id) {
+      this.$q
+        .dialog({
+          dark: true,
+          title: "Confirm",
+          message: "Are you sure you want to delete?",
+          color: "warning",
+          cancel: true,
+          persistent: true,
+        })
+        .onOk(async () => {
+          try {
+
+            db.collection('Tasks').doc(id).delete();
+            this.$q.notify({
+              message: "Delete task",
+              color: "red",
+              textColor: "white",
+              icon: "cloud_done",
+            });
+          } catch (error) {
+            console.log(error);
+          }
+
+          this.tasks.splice(index, 1);
+        });
+    },
+  },
+};
 </script>
 
 <style lang="sass">
